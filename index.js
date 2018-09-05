2a78bb13a0d81d32f49784157df1fac7f0193bd3
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-05 17:50:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ function scuberGreetingForFeet(someValue) {
 
 function ternaryCheckCity(city) {
 const result = (city === "NYC") ? 'Ok, sounds good.' : 'No go.'
+return result
+}
 
+function switchOnCharmFromTip(tip) {
 
 }
