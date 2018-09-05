bbd37bc2521933096f7f7ec25cc4f643ef4ab007
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-05 17:46:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,8 @@ function scuberGreetingForFeet(someValue) {
   } else if (someValue > 2000) {
     result = "I will gladly take your thirty bucks."
   }
+  else if (someValue > 2500) {
+    result = "No can do."
+  }
   return result
 }
